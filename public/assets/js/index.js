9b9f1f8aefd2739d16b51a0b3088b45b69c26531
scicluna <88948700+scicluna@users.noreply.github.com>
2023-02-10 18:17:07 -0500

    How do i even test heroku

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,6 +4,8 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
+console.log("Is the script even detected? Like WTF HEROKU")
+
 if (window.location.pathname === '/notes/') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
