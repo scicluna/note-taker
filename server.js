77a4dc505d5e53b846eb85f6435d41b000bfd9f6
scicluna <88948700+scicluna@users.noreply.github.com>
2023-02-10 17:23:03 -0500

    Fixed ports for Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const notes = require("./routes/notes")
 const notesapi = require("./routes/apinotes")
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
