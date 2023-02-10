4f860bb888f81d6c0f3de9aa98553ad009eae679
scicluna <88948700+scicluna@users.noreply.github.com>
2023-02-10 18:30:02 -0500

    Fixed some local machine errors with the slash at the end of the URL

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -6,7 +6,7 @@ let noteList;
 
 console.log("Is the script even detected? Like WTF HEROKU")
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/notes' || window.location.pathname === '/notes/') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
@@ -122,7 +122,7 @@ const handleRenderSaveBtn = () => {
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
 
-  if (window.location.pathname === '/notes') {
+  if (window.location.pathname === '/notes' || window.location.pathname === '/notes/') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
 
@@ -168,7 +168,7 @@ const renderNoteList = async (notes) => {
     noteListItems.push(li);
   });
 
-  if (window.location.pathname === '/notes') {
+  if (window.location.pathname === '/notes' || window.location.pathname === '/notes/') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
 };
@@ -176,7 +176,7 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/notes' || window.location.pathname === '/notes/') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
